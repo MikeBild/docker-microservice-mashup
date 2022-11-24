c5c0ca7769887f5416341cefa6ef009a8b7b625d
Daniel Schwan <daniel.schwan@exxcellent.de>
2022-11-24 12:28:16 +0100

    implement warenkorb service

---
+++ packages/warenkorb/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import {getWarenkorbFile, writeWarenkorbFile} from "./warenkorbService";
+import Warenkorb from "./warenkorb";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.put("/warenkorb/:id", (req, res) => {
+
+    const warenkorb: Warenkorb = {
+        id: req.params["id"],
+        produkte: req.body["produkte"],
+        zeitStempel: new Date()
+    }
+    writeWarenkorbFile(warenkorb);
+    res.send(JSON.stringify(warenkorb));
+});
+
+app.get("/warenkorb/:id", (req, res) => {
+    try {
+        const warenkorb = getWarenkorbFile(req.params["id"]);
+        res.send(warenkorb);
+    } catch(err) {
+        res.status(404).send({"message": "Warenkorb nicht gefunden."});
+    }
+});
+
+app.listen(process.env.PORT || 4567, () => {
+    console.log(`Listen on 4567!`);
+});
